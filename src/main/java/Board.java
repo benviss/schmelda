a52992f35add36e669967e829ed1c9a6881662b5
Ben Vissotzky and Matt Nicholas <matt@>
2016-10-03 11:09:44 -0700

    add levels that can be called given current level

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -27,6 +27,64 @@ public class Board extends JPanel {
     private int levelCount = 1;
     private boolean completed = false;
 
+    public Board() {
+      addKeyListener(new TAdapter());
+      setFocusable(true);
+      initWorld();
+    }
 
+    public int getBoardWidth() {
+      return this.w;
+    }
 
+    public int getBoardHeight() {
+      return this.h;
+    }
+
+    public final void initWorld() {
+      int x = OFFSET;
+      int y = OFFSET;
+
+      Wall wall;
+      Area a;
+      Warp warp;
+      Water w;
+
+      String currentLevel = Level.getLevel(levelCount)
+
+      for (int i = 0; i < currentLevel.length(); i++) {
+        char item = currentLevel.charAt(i);
+
+        if (item == '\n') {
+          y += SPACE;
+          if (this.w < x) {
+              this.w = x;
+          }
+          x = OFFSET;
+        } else if (item == '#') {
+            wall = new Wall(x, y);
+            collidables.add(wall);
+            x += SPACE;
+        } else if (item == 'w') {
+            w = new Water(x, y);
+            collidables.add(w);
+            x += SPACE;
+        } else if (item == '.') {
+            a = new Area(x, y);
+            areas.add(a);
+            x += SPACE;
+        } else if (item == '?') {
+            warp = new Warp(x, y);
+            warps.add(warp);
+            x += SPACE;
+        } else if (item == '@') {
+            chain = new Player(playerX, playerY);
+            x += SPACE;
+        } else if (item == ' ') {
+            x += SPACE;
+        }
+
+      }
+
+    }
 }

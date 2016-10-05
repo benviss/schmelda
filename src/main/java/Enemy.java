28d0bd0ce2e12eef4743baa8808784b955f016c5
Ben Vissotzky and Matt Nicholas <matt@>
2016-10-05 14:06:47 -0700

    add enemy death

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.net.URL;
 import javax.swing.*;
@@ -8,193 +7,169 @@ import java.util.TimerTask;
 import java.util.Random;
 
 public class Enemy extends Actor {
+  Random myRandomGenerator = new Random();
+  public Timer timer;
+  public Timer timer2 = new Timer();
+  private boolean inverse = false;
+  private int stepCounter;
+  private int mId;
+  private int ruld = 0;
+  private int calc = 3;
+  // private int speed;
 
-public Timer timer;
-public Timer timer2 = new Timer();
-private int stepCounter;
-private boolean inverse = false;
-private int ruld = 0;
-private int calc = 3;
-Random myRandomGenerator = new Random();
-
-    public Enemy(int x, int y) {
-        super(x, y);
-
-        timer = new Timer();
-
-        this.collidable = true;
-        URL loc = this.getClass().getResource("images/blueBlob.gif");
-        ImageIcon iia = new ImageIcon(loc);
-        Image image = iia.getImage();
-        this.setImage(image);
+  public Enemy(int x, int y, int type, int _id) {
+    super(x, y);
+    mId = _id;
+    timer = new Timer();
+    if (type == 1) {
+      this.collidable = true;
+      URL loc = this.getClass().getResource("images/blueBlob.gif");
+      ImageIcon iia = new ImageIcon(loc);
+      Image image = iia.getImage();
+      this.setImage(image);
+    } else if(type == 2) {
+      this.collidable = true;
+      URL loc = this.getClass().getResource("images/camoBlob.gif");
+      ImageIcon iia = new ImageIcon(loc);
+      Image image = iia.getImage();
+      this.setImage(image);
     }
+  }
+  public int getId(){
+    return mId;
+  }
 
-    public Enemy(int x, int y, boolean isHard) {
-        super(x, y);
+  public void move(int x, int y) {
+    int nx = this.x() + x;
+    int ny = this.y() + y;
+    this.setX(nx);
+    this.setY(ny);
+  }
 
-        timer = new Timer();
-
-        this.collidable = true;
-        URL loc = this.getClass().getResource("images/camoBlob.gif");
-        ImageIcon iia = new ImageIcon(loc);
-        Image image = iia.getImage();
-        this.setImage(image);
-    }
-
-
-    public void move(int x, int y) {
-        int nx = this.x() + x;
-        int ny = this.y() + y;
-        this.setX(nx);
-        this.setY(ny);
-    }
-
-    public void leftRightMove(int speed){
-      TimerTask timerTask = new TimerTask(){
-        @Override
-        public void run() {
-
-          if(inverse){
-            move(1,0);
-          }
-          else {
-            move(-1,0);
-          }
-          stepCounter += 1;
-
-          if( stepCounter > 100)
-          {
-            stepCounter = 0;
-            inverse = !inverse;
-          }
-
+  public void leftRightMove(int speed){
+    TimerTask timerTask = new TimerTask(){
+      @Override
+      public void run() {
+        if(inverse){
+          move(1,0);
         }
-      };
-      this.timer.schedule(timerTask, 0, speed);
-    }
-
-    public void upDownMove(int speed){
-      TimerTask timerTask = new TimerTask(){
-        @Override
-        public void run() {
-
-          if(inverse){
-            move(0,1);
-          }
-          else {
-            move(0,-1);
-          }
-          stepCounter += 1;
-
-          if( stepCounter > 100)
-          {
-            stepCounter = 0;
-            inverse = !inverse;
-          }
-
+        else {
+          move(-1,0);
         }
-      };
-      this.timer.schedule(timerTask, 0, speed);
-    }
-
-    public void circleMove(int speed){
-      TimerTask timerTask = new TimerTask(){
-        @Override
-        public void run() {
-
-          if(ruld == 0){
-            move(1,0);
-          }
-          else if(ruld == 1){
-            move(0,-1);
-          }
-          else if(ruld == 2){
-            move(-1,0);
-          }
-          else{
-            move(0,1);
-          }
-
-          if( stepCounter > 100)
-          {
-            ruld += 1;
-
-            if(ruld == 4){
-              ruld = 0;
-            }
-
-            stepCounter = 0;
-          }
-
-          stepCounter += 1;
+        stepCounter += 1;
+        if( stepCounter > 100)
+        {
+          stepCounter = 0;
+          inverse = !inverse;
         }
-      };
-      this.timer.schedule(timerTask, 0, speed);
-    }
+      }
+    };
+    this.timer.schedule(timerTask, 0, speed);
+  }
 
-    public void leftRightMoveRandom(int max){
-      TimerTask timerTask = new TimerTask(){
-
-        @Override
-        public void run() {
-          if(inverse){
-            move(calc,0);
-            stepCounter += calc;
-          }
-          else {
-            move(-calc,0);
-            stepCounter += calc;
-          }
-
-          if( stepCounter > 100)
-          {
-            stepCounter = 0;
-            inverse = !inverse;
-            calc = myRandomGenerator.nextInt(max) + 1;
-          }
+  public void upDownMove(int speed){
+    TimerTask timerTask = new TimerTask(){
+      @Override
+      public void run() {
+        if(inverse){
+          move(0,1);
         }
-      };
-      System.out.println(calc);
-      this.timer2.schedule(timerTask, 0, 50);
-    }
-
-    public void circleMoveRandom(int speed){
-      TimerTask timerTask = new TimerTask(){
-        @Override
-        public void run() {
-
-          if(ruld == 0){
-            move(calc,0);
-            stepCounter += calc;
-          }
-          else if(ruld == 1){
-            move(0,-calc);
-            stepCounter += calc;
-          }
-          else if(ruld == 2){
-            move(-calc,0);
-            stepCounter += calc;
-          }
-          else{
-            move(0,calc);
-            stepCounter += calc;
-          }
-
-          if( stepCounter > 100)
-          {
-            ruld += 1;
-
-            if(ruld == 4){
-              ruld = 0;
-            }
-
-            stepCounter = 0;
-            calc = myRandomGenerator.nextInt(speed) + 1;
-          }
-
+        else {
+          move(0,-1);
         }
-      };
-      this.timer.schedule(timerTask, 0, 50);
-    }
+        stepCounter += 1;
+        if( stepCounter > 100)
+        {
+          stepCounter = 0;
+          inverse = !inverse;
+        }
+      }
+    };
+    this.timer.schedule(timerTask, 0, speed);
+  }
 
+  public void circleMove(int speed){
+    TimerTask timerTask = new TimerTask(){
+      @Override
+      public void run() {
+        if(ruld == 0){
+          move(1,0);
+        }else if(ruld == 1){
+          move(0,-1);
+        }else if(ruld == 2){
+          move(-1,0);
+        }
+        else{
+          move(0,1);
+        }
+        if( stepCounter > 100)
+        {
+          ruld += 1;
+          if(ruld == 4){
+            ruld = 0;
+          }
+          stepCounter = 0;
+        }
+        stepCounter += 1;
+      }
+    };
+    this.timer.schedule(timerTask, 0, speed);
+  }
+
+  public void leftRightMoveRandom(int max){
+    TimerTask timerTask = new TimerTask(){
+      @Override
+      public void run() {
+        if(inverse){
+          move(calc,0);
+          stepCounter += calc;
+        }
+        else {
+          move(-calc,0);
+          stepCounter += calc;
+        }
+        if( stepCounter > 100)
+        {
+          stepCounter = 0;
+          inverse = !inverse;
+          calc = myRandomGenerator.nextInt(max) + 1;
+        }
+      }
+    };
+    System.out.println(calc);
+    this.timer2.schedule(timerTask, 0, 50);
+  }
+
+  public void circleMoveRandom(int speed){
+    TimerTask timerTask = new TimerTask(){
+      @Override
+      public void run() {
+
+        if(ruld == 0){
+          move(calc,0);
+          stepCounter += calc;
+        }else if(ruld == 1){
+          move(0,-calc);
+          stepCounter += calc;
+        }else if(ruld == 2){
+          move(-calc,0);
+          stepCounter += calc;
+        }
+        else{
+          move(0,calc);
+          stepCounter += calc;
+        }
+        if( stepCounter > 100)
+        {
+          ruld += 1;
+          if(ruld == 4){
+            ruld = 0;
+          }
+          stepCounter = 0;
+          calc = myRandomGenerator.nextInt(speed) + 1;
+        }
+      }
+    };
+    this.timer.schedule(timerTask, 0, 50);
+  }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -16,7 +16,7 @@ public class Player extends Actor{
   private final String movingLeft = "images/chain-left.gif";
   private final String idleImg = "images/chain-down.gif";
 
-  private final String attackingimg = "images/attack.gif";
+  private final String attackingImg = "images/attack.gif";
   private String movementDirection = "images/chain-down.gif";
 
   private boolean attacking = false;
@@ -33,118 +33,114 @@ public class Player extends Actor{
   private String moveDirection = "Idle";
   private ArrayList collidables = new ArrayList();
 
-    public Player(int x, int y) {
-        super(x, y);
+  public Player(int x, int y) {
+    super(x, y);
 
-        this.collidable = true;
-        URL loc = this.getClass().getResource(movementDirection);
-        ImageIcon iia = new ImageIcon(loc);
-        Image image = iia.getImage();
-        this.setImage(image);
+    this.collidable = true;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+
+  public boolean getAttacking(){
+    return attacking;
+  }
+  public void setMovement(String _direction) {
+    this.moveDirection = _direction;
+  }
+
+  public void setArrayList(ArrayList collidable) {
+    this.collidables.addAll(collidable);
+  }
+
+  public void setDx(int _new) {
+    this.dx = _new;
+  }
+  public void setDy(int _new) {
+    this.dy = _new;
+  }
+
+  // public class myTimer implements ActionListener {
+  //   public void actionPerformed(ActionEvent evt) {
+  //         this.attacking = false;
+  //         this.setMovingDown();
+  //
+  //   }
+  // }
+
+  public void attacking() {
+    if(lastAttack + attackCooldown < System.currentTimeMillis()){
+      lastAttack = System.currentTimeMillis();
+      this.setAttackingImg();
+      this.attacking =true;
     }
-
-
-    public void setMovement(String _direction) {
-      this.moveDirection = _direction;
+  }
+  public void checkAttack(){
+    if(System.currentTimeMillis() - lastAttack > attackCooldown){
+      this.setIdleImg();
+      this.attacking = false;
     }
-
-    public void setArrayList(ArrayList collidable) {
-      this.collidables.addAll(collidable);
-    }
-
-    public void setDx(int _new) {
-      this.dx = _new;
-    }
-    public void setDy(int _new) {
-      this.dy = _new;
-    }
-
-    // public class myTimer implements ActionListener {
-    //   public void actionPerformed(ActionEvent evt) {
-    //         this.attacking = false;
-    //         this.setMovingDown();
-    //
-    //   }
-    // }
-
-    public void attacking() {
-
-      if(lastAttack + attackCooldown < System.currentTimeMillis()){
-        lastAttack = System.currentTimeMillis();
-        this.setAttackingImg();
-        this.attacking =true;
-
+  }
+  public void setAttackingImg(){
+    movementDirection = attackingImg;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+  public void setMovingUp(){
+    movementDirection = movingUp;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+  public void setMovingRight(){
+    movementDirection = movingRight;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+  public void setMovingDown(){
+    movementDirection = movingDown;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+  public void setMovingLeft(){
+    movementDirection = movingLeft;
+    URL loc = this.getClass().getResource(movementDirection);
+    ImageIcon iia = new ImageIcon(loc);
+    Image image = iia.getImage();
+    this.setImage(image);
+  }
+  public void setIdleImg(){
+    movementDirection = idleImg;
+  }
+  public void move() {
+    if (this.moveDirection.equals("Up")) {
+      if(!this.checkCollidable(collidables,"Up")) {
+        this.setX(this.x() + dx);
+        this.setY(this.y() + dy);
+      }
+    } else if (this.moveDirection.equals("Down")) {
+      if(!this.checkCollidable(collidables,"Down")) {
+        this.setX(this.x() + dx);
+        this.setY(this.y() + dy);
+      }
+    } else if (this.moveDirection.equals("Left")) {
+      if(!this.checkCollidable(collidables,"Left")) {
+        this.setX(this.x() + dx);
+        this.setY(this.y() + dy);
+      }
+    } else if (this.moveDirection.equals("Right")) {
+      if(!this.checkCollidable(collidables,"Right")) {
+        this.setX(this.x() + dx);
+        this.setY(this.y() + dy);
       }
     }
-    public void checkAttack(){
-      if(System.currentTimeMillis() - lastAttack > attackCooldown){
-        this.setIdleImg();
-        this.attacking = false;
-      } 
-    }
-    public void setAttackingImg(){
-      movementDirection = attackingimg;
-      URL loc = this.getClass().getResource(movementDirection);
-      ImageIcon iia = new ImageIcon(loc);
-      Image image = iia.getImage();
-      this.setImage(image);
-    }
-    public void setMovingUp(){
-      movementDirection = movingUp;
-      URL loc = this.getClass().getResource(movementDirection);
-      ImageIcon iia = new ImageIcon(loc);
-      Image image = iia.getImage();
-      this.setImage(image);
-    }
-    public void setMovingRight(){
-      movementDirection = movingRight;
-      URL loc = this.getClass().getResource(movementDirection);
-      ImageIcon iia = new ImageIcon(loc);
-      Image image = iia.getImage();
-      this.setImage(image);
-    }
-    public void setMovingDown(){
-      movementDirection = movingDown;
-      URL loc = this.getClass().getResource(movementDirection);
-      ImageIcon iia = new ImageIcon(loc);
-      Image image = iia.getImage();
-      this.setImage(image);
-    }
-    public void setMovingLeft(){
-      movementDirection = movingLeft;
-      URL loc = this.getClass().getResource(movementDirection);
-      ImageIcon iia = new ImageIcon(loc);
-      Image image = iia.getImage();
-      this.setImage(image);
-    }
-    public void setIdleImg(){
-      movementDirection = idleImg;
-    }
-
-
-    public void move() {
-      if (this.moveDirection.equals("Up")) {
-        if(!this.checkCollidable(collidables,"Up")) {
-          this.setX(this.x() + dx);
-          this.setY(this.y() + dy);
-        }
-      } else if (this.moveDirection.equals("Down")) {
-        if(!this.checkCollidable(collidables,"Down")) {
-          this.setX(this.x() + dx);
-          this.setY(this.y() + dy);
-        }
-      } else if (this.moveDirection.equals("Left")) {
-        if(!this.checkCollidable(collidables,"Left")) {
-          this.setX(this.x() + dx);
-          this.setY(this.y() + dy);
-        }
-      } else if (this.moveDirection.equals("Right")) {
-        if(!this.checkCollidable(collidables,"Right")) {
-          this.setX(this.x() + dx);
-          this.setY(this.y() + dy);
-        }
-      } else {
-
-      }
-    }
+  }
 }
